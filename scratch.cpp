2327438b83ee0ceed8809039b6288ea5e2dcc02f
Chengze Fan <fancz2002@gmail.com>
2018-06-13 09:41:54 +0800

    helloworld

---
+++ scratch.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+using namespace std;
+
+
+int main() {
+    int N;
+    cin >> N;
+    // narrow down the candidate size to 1
+
+    return 0;
+}
