be79938c69b61d4a490c092c5dab4880eea9840a
Chengze Fan <fancz2002@gmail.com>
2018-06-16 11:37:46 +0800

    fixed

---
+++ leetcode.cpp
@@ -0,0 +1,36 @@
+#define LOCAL
+
+#ifdef LOCAL  
+#endif
+#include <iostream> 
+#include <cstdio> 
+#include <fstream> 
+#include <algorithm> 
+#include <cmath> 
+#include <deque> 
+#include <vector> 
+#include <queue> 
+#include <string> 
+#include <cstring> 
+#include <map> 
+#include <stack> 
+#include <set> 
+#include <climits>
+using namespace std;
+
+class Solution{
+private:
+
+public:
+    void solve(){
+
+    }
+};
+
+int main() {
+    Solution a;
+    /* Construct Test Data */
+
+    /* Sovle */
+    a.solve();
+}
---
+++ oj.cpp
@@ -0,0 +1,31 @@
+#define LOCAL
+
+#ifdef LOCAL  
+#endif
+#include <iostream> 
+#include <cstdio> 
+#include <fstream> 
+#include <algorithm> 
+#include <cmath> 
+#include <deque> 
+#include <vector> 
+#include <queue> 
+#include <string> 
+#include <cstring> 
+#include <map> 
+#include <stack> 
+#include <set> 
+#include <climits>
+using namespace std;
+
+int n, k;
+
+int main() {
+
+    #ifdef LOCAL  
+        freopen("testdata.txt","r",stdin);  
+        // freopen("data.out.txt","w",stdout);  
+    #endif  
+    
+    return 0;
+}
